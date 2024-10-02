f2266443bac27ab688d1fa838559b8135a3783ac
Sundarkhanal <112654909+Sundarkhanal@users.noreply.github.com>
2024-10-02 11:14:37 +0545

    Update JavaPractical2.java

--- JavaPractical2.java
+++ JavaPractical2.java
@@ -1,3 +1,8 @@
 public class JavaPractical2 {
+    public static void main(String[] args) {
+        // Print Hello, World! to the console
+        System.out.println("Hello, World!");
+    }
+}
     
 }
